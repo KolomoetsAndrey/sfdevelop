4824353b7b84f347dd442a45b455a5943af0995c
Andrey Kolomoets <kolomoets.and@gmail.com>
2019-04-25 00:37:45 +0300

    main

---
+++ js/main.js
@@ -0,0 +1,27 @@
+function openNav() {
+    var x = window.matchMedia("(max-width:900px)");
+
+    if (x.matches) {
+        document.getElementById("mySidepanel").style.width = "85%";
+    } else {
+        document.getElementById("mySidepanel").style.width = "35%";
+    }
+}
+    
+function closeNav() {
+    document.getElementById("mySidepanel").style.width = "0";
+}
+
+function openPhone() {
+    var x = window.matchMedia("(max-width:900px)");
+
+    if (x.matches) {
+        document.getElementById("myOpenphone").style.width = "85%";
+    } else {
+        document.getElementById("myOpenphone").style.width = "35%";
+    }
+}
+    
+function closePhone() {
+    document.getElementById("myOpenphone").style.width = "0";
+}
